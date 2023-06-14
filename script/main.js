85ccb742f16c8cf86f65e72beac7d5d85b6944db
RyoAizawa <aizawaryou6419@gmail.com>
2023-06-14 17:53:48 +0900

    実務課題03を実施

--- script/main.js
+++ script/main.js
@@ -7,7 +7,13 @@ Vue.createApp({
       filteredCards: [], // ソート、検索結果を入れる社員情報
       searchSelect: "default",
       searchText: "",
-      error: false,
+      addName: "",
+      addCompany: "",
+      addDivision: "",
+      addTitle: "",
+      lastedUserId: 0,
+      searchError: false,
+      addErrorMsg: "",
     };
   },
   created: async function () {
@@ -16,13 +22,37 @@ Vue.createApp({
     const cards = await res.json();
     // 初期値の社員情報はコンポーネント内に保持
     this.orgCards = JSON.parse(JSON.stringify(cards));
+    // 初期値の社員情報の末尾のユーザーIDを取得。
+    for (index in this.orgCards) this.lastedUserId = this.orgCards[index]["id"];
     // フィルター済みの社員情報を出力する
     this.filteredCards = JSON.parse(JSON.stringify(cards));
   },
   methods: {
-    sortBy(key, callSearch = false) {
+    initializeFilteredCards() {
+      // フィルター済みリストを初期化し、オリジナルのリストをコピー
+      this.filteredCards = [];
+      this.filteredCards = JSON.parse(JSON.stringify(this.orgCards));
+    },
+
+    checkReturnToDefault() {
+      // 標準を選択し検索した場合, または標準に戻る場合の処理
+      if (this.searchSelect === "default") {
+        // filteredCardsの初期化
+        this.initializeFilteredCards();
+        // 入力されている検索語句は削除
+        this.searchText = "";
+        // ソートのデータにはnullを代入してaddClassで追加されている矢印を消す
+        this.sortOrder = null;
+        // エラーメッセージが出ている場合は非表示にする。
+        this.searchError = false;
+        return false;
+      }
+      return true;
+    },
+
+    sortBy(key, update = false) {
       // 前回昇順なら降順、前回降順なら昇順にする。検索からの呼び出しならそのまま。
-      if (!callSearch) {
+      if (!update) {
         this.sortOrder === 0 ? (this.sortOrder = 1) : (this.sortOrder = 0);
       }
       // 前回と違うキーを選択した場合、または検索の場合は昇順からスタート
@@ -57,6 +87,7 @@ Vue.createApp({
       // 今回ソートを行ったキーを保持
       this.sortKey = key;
     },
+
     addClass(key) {
       return {
         // 今回ソートを行い、昇順の場合クラス名「asc」を返す
@@ -65,27 +96,16 @@ Vue.createApp({
         desc: this.sortKey === key && this.sortOrder === 1,
       };
     },
-    search() {
-      // 標準を選択し検索した場合の処理
-      if (this.searchSelect === "default") {
-        // フィルター済みの社員情報を初期化
-        this.filteredCards = [];
-        // オリジナルの社員情報をコピー
-        this.filteredCards = JSON.parse(JSON.stringify(this.orgCards));
-        // 入力されている検索語句は削除
-        this.searchText = "";
-        // ソートのデータにはnullを代入してaddClassで追加されている矢印を消す
-        this.sortOrder = null;
-        return;
-      }
 
+    search() {
+      // 「標準」が選択されていたらに戻すかチェック。標準だったら何もせず関数終了。
+      if (!this.checkReturnToDefault()) return;
       // 検索語句が何も入力されていない場合エラーを出力し関数終了
       if (this.searchText === "") {
-        this.error = true;
+        this.searchError = true;
         return;
-      } else {
-        this.error = false;
       }
+      this.searchError = false;
       // フィルター済みの社員情報を初期化
       this.filteredCards = [];
       // オリジナルの社員情報分処理を繰り返す
@@ -110,6 +130,46 @@ Vue.createApp({
         this.sortBy(this.sortKey, true);
         this.addClass(this.sortKey);
       }
+    },
+
+    addUser() {
+      // 名前は記号の入力は受け付けない
+      let nameVal = /^[0-9０-９!?_+*'"`#$%&\-^\\@;:,./=~|[\](){}<>！？＿＋＊’”‘＃＄％＆￥－＾￥＠；：，．／＝～｜［］（）｛｝＜＞]+$/;
+      if (this.addName === "" || this.addName.match(nameVal) !== null) {
+        this.addErrorMsg = "名前を入力してください(記号は使用不可)";
+        return;
+      } else if (this.addName.indexOf(" ") < 0) {
+        this.addErrorMsg = "苗字と名前は半角スペースで区切ってください。"
+        return;
+      } else if (this.addCompany === "") {
+        this.addErrorMsg = "会社名を入力してください。";
+        return;
+      } else if (this.addDivision === "") {
+        this.addErrorMsg = "部署名を入力してください。";
+        return;
+      } else {
+        this.addErrorMsg = "";
+      }
+
+      // 入力された値でオブジェクトを作成する
+      addObj = {};
+      addObj["id"] = this.lastedUserId += 1;
+      addObj["name"] = this.addName;
+      addObj["company"] = this.addCompany;
+      addObj["division"] = this.addDivision;
+      addObj["title"] = this.addTitle;
+      // オリジナルリストの末尾にユーザー情報を追加
+      this.orgCards.push(addObj);
+
+      // ブラウザ出力されている方のリストも更新して「標準」に戻す
+      this.searchSelect = "default";
+      this.checkReturnToDefault();
+
+      // テキストボックスを空にする
+      this.addName = "";
+      this.addCompany = "";
+      this.addDivision = "";
+      this.addTitle = "";
     },
   },
 }).mount("#app");
